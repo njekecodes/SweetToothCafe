d4ae9e83d4f5f43eed1b79af7a2143aa248b3a5f
Njeke Codes <njekesdesigns@gmail.com>
2023-11-22 01:20:18 +0300

    Updated urls to include media files

--- SweetToothCafe/urls.py
+++ SweetToothCafe/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from SweetToothCafe import settings
 from sweet_tooth_cafe import views
 
 urlpatterns = [
@@ -36,4 +38,4 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('wishlist/', views.wishlist, name='wishlist'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
